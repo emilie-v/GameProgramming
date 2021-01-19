28ef8cd95a1b69794740292f7d1c3f383ea84215
Emilie <emivog0223@skola.goteborg.se>
2021-01-19 16:33:51 +0100

    yes!

--- _01Trig/L01_Circle/build-tmp/source/L01_Circle.java
+++ _01Trig/L01_Circle/build-tmp/source/L01_Circle.java
@@ -14,9 +14,6 @@ import java.io.IOException;
 
 public class L01_Circle extends PApplet {
 
-int frame = 0;
-int numberOfPoints = 100;
-int distance = 30;
 PVector centerPoint = new PVector();
 float offset;
 
@@ -24,35 +21,59 @@ public void setup()
 {
 	
 	strokeWeight(6);
-	stroke(54, 117, 136);
+	
 	centerPoint = new PVector (width / 2, height / 2);
-	numberOfPoints = 0;
-	frame++;
 }
 
 public void draw()
 {
 	background(0);
-	numberOfPoints = 100;
 	circleCurve();
-	offset = millis() * 0.001f;
+	spiral();
+	offset = millis() * 0.004f;
 
-frame++;
 }
-
 
 public void circleCurve()
 {
-	float radius = 150;
+	stroke(54, 117, 136);
+	int circlePoints = 100;
+	float radius = 232;
 	float angle = 0;
-	float slice = PI * 2 / numberOfPoints;
+	float slice = PI * 2 / circlePoints;
 
-for (int i = 0; i < numberOfPoints; i++)
+for (int i = 0; i < circlePoints; i++)
 	{
 		angle = i * slice;
 		float x = centerPoint.x + cos(angle + offset) * radius;
 		float y = centerPoint.y + sin(angle + offset) * radius;
 		point (x, y);	
+		//line (x, y, centerPoint.x, centerPoint.y);
+	}
+}
+
+public void spiral()
+{
+	int spiralPoints = 104;
+	float radius = 234;
+	float angle = 0;
+	float slice = PI * 5 / spiralPoints;
+
+	int from = color (179, 59, 245);
+	int to = color (54, 119, 136);
+
+	float colourIncrement = (float) spiralPoints / (spiralPoints * spiralPoints);
+	println(colourIncrement);
+
+	for (int i = 0; i < spiralPoints; i++) 
+	{
+	 	stroke (lerpColor(from, to, colourIncrement * i));
+
+		float distance = ((float) i / spiralPoints);
+		angle = i * slice;
+		float x = centerPoint.x + cos(angle + offset) * radius * distance;
+		float y = centerPoint.y + sin(angle + offset) * radius * distance;
+		point (x, y);
 	}
 }
   public void settings() { 	size (640, 480); }
