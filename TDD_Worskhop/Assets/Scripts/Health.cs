fdbaa3b1a84cf8f64bdeccc87c24b75c42bd79df
Emilie <emivog0223@skola.goteborg.se>
2021-02-15 14:32:31 +0100

    create

---
+++ TDD_Worskhop/Assets/Scripts/Health.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health
+{
+    public int health;
+
+    public void Add(int v)
+    {
+        health += v;
+    }
+
+    public void RemoveHealth(int v)
+    {
+        health -= v;
+    }
+}
---
+++ TDD_Worskhop/Assets/Tests/EditMode/HealthTests.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class HealthTests
+{
+    Health health;
+
+    [SetUp]
+    public void SetUp()
+    {
+        health = new Health();
+        health.health = 100;
+    }
+
+    [TearDown]
+    public void TearDown()
+    {
+        health.health = 0;
+        health = null;
+    }
+
+    [Test]
+    public void CanAddHealth()
+    {
+        health.Add(100);
+        Assert.AreEqual(200, health.health);
+    }
+
+    [Test]
+    public void CanRemoveHealth()
+    {
+        health.RemoveHealth(20);
+        Assert.AreEqual()
+    }
+
+}
