cd4d236ea04753560015f7bfa3d29b975c59b6d6
Emilie <emivog0223@skola.goteborg.se>
2021-04-15 21:25:07 +0200

    Spinwheel

---
+++ Candyland_remix/CandyLand - Em's remix!/Assets/Scripts/Spinwheel.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Spinwheel : MonoBehaviour
+{
+    private Rigidbody2D rb2D;
+
+    [SerializeField] private Slider forceSlider;
+    [SerializeField] private Button spinButton;
+    public void Spin()
+    {
+        spinButton.interactable = false; 
+        rb2D.AddTorque(forceSlider.value, ForceMode2D.Impulse);
+    }
+    void Start()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+    }
+    
+    void Update()
+    {
+        if (rb2D.angularVelocity > 0)
+        {
+            return;
+        }
+
+        spinButton.interactable = true;
+    }
+}
